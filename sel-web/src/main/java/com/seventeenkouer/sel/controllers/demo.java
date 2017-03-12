6f96a1d8003a3f7f6249e026ec92e56cbb483f4c
philhong <philhong2012@gmail.com>
2017-03-12 21:01:53 +0800

    add jdbc config

---
+++ sel-web/src/main/java/com/seventeenkouer/sel/controllers/demo.java
@@ -0,0 +1,9 @@
+package com.seventeenkouer.sel.controllers;
+
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+/**
+ * Created by wind on 17/3/12.
+ */
+public class demo {
+}
