ef35606224e826b6b9c3762535601d42f9227698
hongxubing <hongxb@zdnst.com>
2017-03-28 17:55:29 +0800

    访问着模式，真的好晦涩。。。。。。

--- common/src/main/java/com/seventeenkouer/designpatten/visitor/VisitorClient.java
+++ common/src/main/java/com/seventeenkouer/designpatten/visitor/VisitorClient.java
@@ -5,6 +5,10 @@ package com.seventeenkouer.designpatten.visitor;
  * 演示访问者模式
  * 不变因子：数据结构必须是稳定的即BaseElement的子类是确定的
  * 变化因子：访问者，例如访问者是一个状态 ，1，2，3，4，对应1，2，3，4 去实现数据结构在4个状态下的行为逻辑
+ * 将数据结构与算法剥离
+ * 适用于数据结构稳定，而算法又多变的系统，访问者模式使得增加算法操作变得容易，缺点就是数据结构不能随意变动，数据结构变化，那么访问者的接口是不稳定的
+ * 访问着模式适用于数据结构（一个只有属性的类）稳定的系统
+ *
  */
 public class VisitorClient {
     public static void main(String[] args) {
