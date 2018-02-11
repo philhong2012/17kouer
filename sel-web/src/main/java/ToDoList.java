0f592cf1bb1803b2109c3d9f01d6de5a8392c886
philhong <philhong2012@gmail.com>
2018-02-11 17:55:46 +0800

    更新todolist

--- sel-web/src/main/java/ToDoList.java
+++ sel-web/src/main/java/ToDoList.java
@@ -6,7 +6,7 @@
  **/
 public class ToDoList {
     //todo: api 接口权限
-    //todo: facade 模块的单元测试
+    //todo: facade 模块的单元测试，本来是想把功能写在service层，然后facade层通过组合各个service来实现需要跨service的功能
     //todo: 图片展示功能
     //todo: 将controller一层独立出去做网关，前后端完全分离，考虑如何为移动端提供api
     //todo：与前端联调特别耗时间，如何优化呢
