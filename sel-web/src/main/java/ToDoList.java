5c4b874ee10d580d444f78fdf71f08515a5ba40d
philhong <philhong2012@gmail.com>
2018-02-11 17:19:06 +0800

    增加todolist项

---
+++ sel-web/src/main/java/ToDoList.java
@@ -0,0 +1,15 @@
+/**
+ * 用一个类记录将要做的事情
+ *
+ * @author hongxubing
+ * @create 2018-02-11 17:14
+ **/
+public class ToDoList {
+    //todo: api 接口权限
+    //todo: facade 模块的单元测试
+    //todo: 图片展示功能
+    //todo: 将controller一层独立出去做网关，前后端完全分离，考虑如何为移动端提供api
+    //todo：与前端联调特别耗时间，如何优化呢
+
+
+}
