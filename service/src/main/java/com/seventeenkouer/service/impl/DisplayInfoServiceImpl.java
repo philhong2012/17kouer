572ecf4d93aa94660a0fcf2e3f70c8b847176589
philhong <philhong2012@gmail.com>
2018-09-13 00:18:01 +0800

    增加必填信息校验

--- service/src/main/java/com/seventeenkouer/service/impl/DisplayInfoServiceImpl.java
+++ service/src/main/java/com/seventeenkouer/service/impl/DisplayInfoServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 /**
@@ -24,6 +25,19 @@ public class DisplayInfoServiceImpl implements DisplayInfoService {
     public Integer saveDisplayInfo(DisplayInfo displayInfo) {
         Assert.hasText(displayInfo.getDrugBcode(),"药品条码不能为空");
         Assert.hasText(displayInfo.getDrugName(),"药品名称不能为空");
+
+        Assert.hasText(displayInfo.getDrugCode(),"药品编码不能为空");
+        Assert.hasText(displayInfo.getDrugNumb(),"药品批次不能为空");
+        Assert.hasText(displayInfo.getDispSurf(),"药品陈列面不能为空");
+        Assert.hasText(displayInfo.getDispPosi(),"药品陈列位置不能为空");
+        //Assert.hasText(displayInfo.getDrugPrice(),"药品价格不能为空");
+        //Assert.hasText(displayInfo.getStoreNum(),"药品库存不能为空");
+        Assert.isTrue(displayInfo.getDrugPrice() != null &&
+        displayInfo.getDrugPrice().compareTo(BigDecimal.ZERO) >=0,"药品价格不能为空");
+
+        Assert.isTrue(displayInfo.getStoreNum() != null &&
+                displayInfo.getStoreNum().compareTo(BigDecimal.ZERO) >=0,"药品库存不能为空");
+
         Long currentTime = System.currentTimeMillis();
 
         displayInfo.setCreatetime(new Date(currentTime));
